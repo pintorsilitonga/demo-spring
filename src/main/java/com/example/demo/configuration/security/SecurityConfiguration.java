5fb9c84892877a87529eb7c40a7c4933ce199f69
Pintor Jonathan Silitonga <pintor.silitonga@gdplabs.id>
2019-01-24 14:16:59 +0700

    security config

--- src/main/java/com/example/demo/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/example/demo/configuration/security/SecurityConfiguration.java
@@ -2,12 +2,14 @@ package com.example.demo.configuration.security;
 
 import com.example.demo.DemoProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -27,7 +29,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         httpSecurity
                 .authorizeRequests().antMatchers(graphqlApiBasePath + "/**")
-                    .hasAnyRole("ADMIN", "USER").and()
+                    .permitAll().and()
                 .httpBasic().and()
                 .csrf().disable();
     }
