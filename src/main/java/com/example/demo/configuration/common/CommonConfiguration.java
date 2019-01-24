75fcbbfac4502c49c459cd50b3af5802503b346c
Pintor Jonathan Silitonga <pintor.silitonga@gdplabs.id>
2019-01-24 14:20:44 +0700

    blowfish crypt encoder

---
+++ src/main/java/com/example/demo/configuration/common/CommonConfiguration.java
@@ -0,0 +1,13 @@
+package com.example.demo.configuration.common;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class CommonConfiguration {
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
