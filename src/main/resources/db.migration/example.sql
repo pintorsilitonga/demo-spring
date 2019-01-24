e64d2e55b4dc539d16440d64fb862ca64d487328
Pintor Jonathan Silitonga <pintor.silitonga@gdplabs.id>
2019-01-24 14:22:31 +0700

    query data table authority

---
+++ src/main/resources/db.migration/example.sql
@@ -0,0 +1,9 @@
+INSERT INTO `demo_spring`.`authority` (`id`, `description`, `name`) VALUES ('ADD_COMMENT', 'Add Comment', 'Add Comment');
+INSERT INTO `demo_spring`.`authority` (`id`, `description`, `name`) VALUES ('EDIT_COMMENT', 'Edit Comment', 'Edit Comment');
+INSERT INTO `demo_spring`.`authority` (`id`, `description`, `name`) VALUES ('DELETE_COMMENT', 'Delete Comment', 'Delete Comment');
+INSERT INTO `demo_spring`.`authority` (`id`, `description`, `name`) VALUES ('ADD_ARTICLE', 'Add Article', 'Add Article');
+INSERT INTO `demo_spring`.`authority` (`id`, `description`, `name`) VALUES ('EDIT_ARTICLE', 'Edit Article', 'Edit Article');
+INSERT INTO `demo_spring`.`authority` (`id`, `description`, `name`) VALUES ('DELETE_ARTICLE', 'Delete Article', 'Delete Article');
+INSERT INTO `demo_spring`.`authority` (`id`, `description`, `name`) VALUES ('ADD_PROFILE', 'Add Profile', 'Add Profile');
+INSERT INTO `demo_spring`.`authority` (`id`, `description`, `name`) VALUES ('EDIT_PROFILE', 'Edit Profile', 'Edit Profile');
+INSERT INTO `demo_spring`.`authority` (`id`, `description`, `name`) VALUES ('DELETE_PROFILE', 'Delete Profile', 'Delete Profile');
