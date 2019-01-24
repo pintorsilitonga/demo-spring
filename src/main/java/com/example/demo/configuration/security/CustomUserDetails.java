4ea6b639beb5ee9fb175f204aedafcec5ef26c80
Pintor Jonathan Silitonga <pintor.silitonga@gdplabs.id>
2019-01-24 14:20:55 +0700

    CustomUserDetails

---
+++ src/main/java/com/example/demo/configuration/security/CustomUserDetails.java
@@ -0,0 +1,60 @@
+package com.example.demo.configuration.security;
+
+import com.example.demo.module.profile.Profile;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CustomUserDetails implements UserDetails {
+    private Profile profile;
+    private List<GrantedAuthority> authorities;
+
+    CustomUserDetails(Profile profile) {
+        this.profile = profile;
+        this.authorities =
+                profile.getRole()
+                        .getAuthorities()
+                        .stream()
+                        .map(authority -> new SimpleGrantedAuthority(authority.getId()))
+                        .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return profile.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return profile.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
