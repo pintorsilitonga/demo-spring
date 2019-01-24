53e65544e26a5c7ad045143f04ca18e7bc147e53
Pintor Jonathan Silitonga <pintor.silitonga@gdplabs.id>
2019-01-24 14:19:41 +0700

    update profilerequest adapter for profile input

--- src/main/java/com/example/demo/module/profile/ProfileRequestAdapter.java
+++ src/main/java/com/example/demo/module/profile/ProfileRequestAdapter.java
@@ -1,9 +1,14 @@
 package com.example.demo.module.profile;
 
+import com.example.demo.module.role.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProfileRequestAdapter {
+    @Autowired private BCryptPasswordEncoder passwordEncoder;
+    @Autowired private RoleService roleService;
 
     public Profile toProfile(ProfileRequest request) {
         Profile profile = new Profile();
@@ -17,13 +22,18 @@ public class ProfileRequestAdapter {
         profile.setUsername(request.getUsername());
         profile.setEmail(request.getEmail());
         profile.setBio(request.getBio());
+        profile.setRole(roleService.findById(request.getRoleId()));
+
+        if (request.getPassword() != null && !request.getPassword().equals("")) {
+            profile.setPassword(passwordEncoder.encode(request.getPassword()));
+        }
     }
 
     public void deactivateProfile(Profile profile) {
-        profile.setActive(false);
+        profile.setStatus(ProfileStatus.INACTIVE);
     }
 
     public void activateProfile(Profile profile) {
-        profile.setActive(true);
+        profile.setStatus(ProfileStatus.ACTIVE);
     }
 }
